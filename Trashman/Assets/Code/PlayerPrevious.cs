c4dc09c8c6e683c940004630d7403acdda9c750a
jly1998 <54792997+jly1998@users.noreply.github.com>
2022-02-25 19:31:36 -0600

    update character moving in Test

--- Trashman/Assets/Code/PlayerPrevious.cs
+++ Trashman/Assets/Code/PlayerPrevious.cs
@@ -3,25 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Direction {
-    Up = 0,
-    Down = 1,
-    Left = 2,
-    Right = 3
-}
 
-namespace Trashman {
-    public class PlayerController : MonoBehaviour {
+namespace Trashman
+{
+    public class PlayerPrevious : MonoBehaviour
+    {
         //outlet
         Rigidbody2D _rigidbody2D;
         SpriteRenderer _spriteRenderer;
         BoxCollider2D _boxCollider;
         public Transform[] attackZones;
 
-        float moveSpeed = 5f;
-        float healthLoseSpeed = 10f;
-        float x_direction = 0f;
-        float y_direction = 0f;
+        public Vector2 targetPos = new Vector2(-2.7f, 1.9f);
+        Vector2 lastPos = new Vector2(-2.7f, 1.9f);
 
         // health bar
         public float maxHealth = 40f;
@@ -31,6 +25,12 @@ namespace Trashman {
         public InventoryManager inventory;
 
 
+        float timeElapsed = 0;
+        float lerpDuration = 1;
+
+        float restTime = 0.9f;
+        float restTimer = 0;
+
         Animator _animator;
 
         // State Tracking
@@ -39,7 +39,8 @@ namespace Trashman {
 
 
         // Start is called before the first frame update
-        void Start() {
+        void Start()
+        {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _boxCollider = GetComponent<BoxCollider2D>();
@@ -49,51 +50,93 @@ namespace Trashman {
 
         void FixedUpdate() {
 
+            if (timeElapsed < lerpDuration) {
+                transform.position = Vector2.Lerp(lastPos, targetPos, timeElapsed / lerpDuration);
+                timeElapsed += Time.deltaTime;
+            } else {
+                lastPos = targetPos;
+                transform.position = targetPos;
+                timeElapsed = 0;
+            }
+
+            // Not rest for enough time: not moving
+            restTimer += Time.deltaTime;
+            if (restTimer < restTime)
+                return;
+
             float h = Input.GetAxisRaw("Horizontal");
             float v = Input.GetAxisRaw("Vertical");
-            if (Math.Abs(h) > float.Epsilon) {
+            if (h != 0) {
                 v = 0;
             }
-            if (Math.Abs(v) > float.Epsilon) {
-                h = 0;
-            }
-            // Debug.Log(currentHealth);
-            if (currentHealth > 1e-5) {
-                _rigidbody2D.velocity = new Vector2(h, v) * moveSpeed;
-                if (Math.Abs(h) > float.Epsilon || Math.Abs(v) > float.Epsilon) {
-                    x_direction = h;
-                    y_direction = v;
-                    _animator.SetTrigger("Move");
-                    LoseHealth(Time.deltaTime * healthLoseSpeed);
+            if (currentHealth >= 2f) {
+                if (h != 0 || v != 0) {
 
-                } else {
-                    _animator.ResetTrigger("Move");
+                    _boxCollider.enabled = false;
+                    RaycastHit2D hit = Physics2D.Linecast(targetPos, targetPos + new Vector2(h, v));
+                    _boxCollider.enabled = true;
+                    Debug.Log(hit.collider);
+                    if (hit.collider == null) {
+                        lastPos = targetPos;
+                        targetPos += new Vector2(h, v);
+                        LoseHealth(2f);
+                    } else {
+                        switch (hit.collider.tag) {
+                            case "Wall":
+                                break;
+
+                            case "Food":
+                                // Pickup food - By Hou
+                                inventory.Add(inventory.foods[hit.collider.name]);
+
+                                targetPos += new Vector2(h, v);
+                                Destroy(hit.transform.gameObject);
+                                break;
+
+                            case "Tool":
+                                // Pickup tool - By Hou
+                                inventory.Add(inventory.tools[hit.collider.name]);
+
+                                targetPos += new Vector2(h, v);
+                                Destroy(hit.transform.gameObject);
+                                break;
+                        }
+
+                    }
+                    restTimer = 0;
                 }
-            } else {
-                _rigidbody2D.velocity = new Vector2(0, 0);
-                _animator.ResetTrigger("Move");
             }
-
-            _animator.SetFloat("Way2GoX", x_direction);
-            _animator.SetFloat("Way2GoY", y_direction);
-
-
 
         }
 
 
         // Update is called once per frame
-        void Update() {
+        void Update()
+        {
+
+            if ((Math.Abs(transform.position.x - targetPos.x) < 0.001f) && (Math.Abs(transform.position.y - targetPos.y) < 0.001f)) {
+                _animator.ResetTrigger("Move");
+            } else {
+                _animator.SetTrigger("Move");
+                _animator.SetFloat("Way2GoX", targetPos.x - transform.position.x);
+                _animator.SetFloat("Way2GoY", targetPos.y - transform.position.y);
+            }
 
             // Use item in inventory - By Hou
-            for (int i = 1; i < 10; i++) {
-                if (Input.GetKeyDown("" + i)) {
+            for (int i = 1; i < 10; i++)
+            {
+                if (Input.GetKeyDown("" + i))
+                {
                     ItemClass item = inventory.Get(i);
-                    if (item != null) {
-                        if (item.itemType == "food") {
+                    if (item != null)
+                    {
+                        if (item.itemType == "food")
+                        {
                             item = inventory.Remove(i);
                             GainHealth(((FoodClass)item).GetFood().healthAdded);
-                        } else {
+                        }
+                        else
+                        {
                             //_animator.SetTrigger("attack");
 
                             // Convert enrumeration to an index
@@ -106,19 +149,22 @@ namespace Trashman {
                             Collider2D[] hits = Physics2D.OverlapCircleAll(attackZone.position, 0.1f);
 
                             // Handle each hit target
-                            foreach (Collider2D hit in hits) {
+                            foreach(Collider2D hit in hits) {
                                 //bool knifeFlag = true;
                                 BarrierClass barrier = hit.GetComponent<BarrierClass>();
-                                if (barrier != null) {
+                                if(barrier != null) {
                                     item = inventory.Remove(i);
                                     barrier.Break();
-                                } else {
+                                }
+                                else {
                                     print("There is no barrier to be destroyed.");
                                 }
                             }
                         }
                         break;
-                    } else {
+                    }
+                    else
+                    {
                         //TODO: User try to use non-existing item
                         //TODO: Add detection for whether this item can be used
                         print("Player try to use non-existing item.");
@@ -133,24 +179,6 @@ namespace Trashman {
                 animator.speed = 1f;
             }*/
 
-        }
-
-        void OnCollisionEnter2D( Collision2D other ) {
-            Debug.Log("collision in player");
-            if (other.gameObject.CompareTag("Food")) {
-                Debug.Log("collision with food");
-                // Pickup food - By Hou
-                inventory.Add(inventory.foods[other.gameObject.name]);
-
-                Destroy(other.gameObject);
-            }
-            if (other.gameObject.CompareTag("Tool")) {
-                Debug.Log("collision with tool");
-                // Pickup tool - By Hou
-                inventory.Add(inventory.tools[other.gameObject.name]);
-
-                Destroy(other.gameObject);
-            }
         }
 
         // Health - By Hou
@@ -176,16 +204,15 @@ namespace Trashman {
 
         // Identify the facing direction
         void LateUpdate() {
-            if (String.Equals(_spriteRenderer.sprite.name, "mario_24")) {
+            if(String.Equals(_spriteRenderer.sprite.name, "mario_24")) {
                 facingDirection = Direction.Up;
-            } else if (String.Equals(_spriteRenderer.sprite.name, "mario_1")) {
+            } else if(String.Equals(_spriteRenderer.sprite.name, "mario_1")) {
                 facingDirection = Direction.Down;
-            } else if (String.Equals(_spriteRenderer.sprite.name, "mario_10")) {
+            }else if(String.Equals(_spriteRenderer.sprite.name, "mario_10")) {
                 facingDirection = Direction.Left;
-            } else if (String.Equals(_spriteRenderer.sprite.name, "mario_13")) {
+            }else if(String.Equals(_spriteRenderer.sprite.name, "mario_13")) {
                 facingDirection = Direction.Right;
             }
         }
-
     }
 }
